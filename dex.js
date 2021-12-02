0fa43264b30e5f89b3b8e7a6fb082ec09b85e214
dizid <marc@dizid.com>
2021-12-02 17:26:31 +0100

    Add blockchain network check and report

--- dex.js
+++ dex.js
@@ -148,3 +148,57 @@ document.getElementById("to_token_select").onclick = () => {
 document.getElementById("login_button").onclick = login;
 document.getElementById("from_amount").onblur = getQuote;
 document.getElementById("swap_button").onclick = trySwap;
+
+
+// CHECKCHAIN
+
+var web3;
+checkWeb3();
+
+function displayMessage(messageType, message){
+    messages = {
+        "00":`<div class= "alert alert-success"> ${message} </div>`,
+        "01":`<div class= "alert alert-danger"> ${message} </div>`
+    }
+    document.getElementById("resultSpace").innerHTML = messages[messageType];
+}
+
+async function checkWeb3(){
+    const ethereum = window.ethereum;
+    if(!ethereum || !ethereum.on) {
+        displayMessage("01", "This App Requires MetaMask, Please Install MetaMask");
+    }
+    else{
+        //displayMessage("00", "Metamask is Installed");
+        setWeb3Environment()
+    }
+}
+
+function setWeb3Environment(){
+    web3 = new Web3(window.ethereum);
+    getNetwork();
+    monitorNetwork();
+}
+
+async function getNetwork(){
+    chainID = await web3.eth.net.getId();
+    displayMessage("00","Active network is "+ getNetworkName(chainID));
+}
+
+function getNetworkName(chainID){
+    networks = {
+        1:"Ethereum Mainnet",
+        4:"Ethereum Rinkeby",
+        56:"Binance Smart Chain Mainnet",
+        97:"Binance Smart Chain Testnet",
+        137:"Matic Mainnet",
+        80001:"Polygon Mumbai Testnet"
+    }
+    return networks[chainID];
+}
+
+function monitorNetwork(){
+    Moralis.onChainChanged(function(){
+        window.location.reload()
+    })
+}
